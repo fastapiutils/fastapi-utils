67b1f9410d45473a2e7e57b39414d6f1dcd47f12
Yuval Levi <yuvall9313@gmail.com>
2024-11-10 18:46:25 +0200

    Bump to version 0.8.0

--- fastapi_utils/__init__.py
+++ fastapi_utils/__init__.py
@@ -7,20 +7,20 @@ if sys.version_info.minor < 8:
     import pkg_resources
 
     try:
-        __version__ = pkg_resources.get_distribution(__name__).version
+        __version__ = "0.8.0"
     except pkg_resources.DistributionNotFound as e:
         warnings.warn(f"Could not determine version of {__name__}", stacklevel=1)
         warnings.warn(str(e), stacklevel=1)
-        __version__ = "unknown"
+        __version__ = "0.8.0"
 else:
     import importlib.metadata
 
     try:
-        __version__ = importlib.metadata.version(__name__)
+        __version__ = "0.8.0"
     except importlib.metadata.PackageNotFoundError as e:
         warnings.warn(f"Could not determine version of {__name__}", stacklevel=1)
         warnings.warn(str(e), stacklevel=1)
-        __version__ = "unknown"
+        __version__ = "0.8.0"
 
 
 __all__ = [
